d106e1d31732f7c02e540abd9a484821bf876a70
TiborKoderman <tibor.koderman@gmail.com>
2021-10-02 15:40:39 +0200

    working command manager

---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client, Collection, Intents, Interaction} = require(`discord.js`);
+const { REST } = require(`@discordjs/rest`);
+const { Routes } = require(`discord-api-types/v9`);
+
+const fs = require(`fs`);
+
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]})
+client.commands = new Collection();
+
+let config = JSON.parse(fs.readFileSync(`config.json`));
+
+const rest = new REST({version: 9}).setToken(config.token);
+
+const commandFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith(`.js`));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
+client.on(`interactionCreate`, async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if(!command) return;
+
+    try{
+        await command.execute(interaction);
+    } catch (error){
+        console.error(error);
+        await interaction.reply({content: `There was an error while executing this command!`, ephemeral: true});
+    }
+});
+
+
+
+client.on(`ready`, ()=>{
+    console.log(`Logged in as ${client.user.tag}!`)
+});
+
+
+client.login(config.token);
